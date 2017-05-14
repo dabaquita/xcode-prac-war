2bfe23efab59722a7d1198d192f8444a6a00dd71
dabaquita <dabaquita@gmail.com>
2017-05-14 10:28:32 -0400

    final

--- War/ViewController.swift
+++ War/ViewController.swift
@@ -11,10 +11,17 @@ import UIKit
 class ViewController: UIViewController {
     
     @IBOutlet weak var rightImageView: UIImageView!
+    @IBOutlet weak var rightScoreLabel: UILabel!
+    var rightScore = 0
+    
     @IBOutlet weak var leftImageView: UIImageView!
     @IBOutlet weak var leftScoreLabel: UILabel!
-    @IBOutlet weak var rightScoreLabel: UILabel!
+    var leftScore = 0
 
+
+    let cardNames = ["card2", "card3", "card4", "card5", "card6",
+                     "card7", "card8", "card9", "card10", "jack", "queen", "king", "ace"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -27,17 +34,41 @@ class ViewController: UIViewController {
 
     @IBAction func dealTapped(sender:UIButton) {
         
-        // Random left number from 2 - 10 below
-        let leftNumber = arc4random_uniform(9) + 2
+        // Random left number from 0 to 12
+        let leftNumber:Int = Int(arc4random_uniform(13))
         
         // Set left image
-        leftImageView.image = UIImage(named: "card\(leftNumber)")
+        leftImageView.image = UIImage(named: cardNames[leftNumber])
         
-        // Random right number from 2 - 10
-        let rightNumber = arc4random_uniform(9) + 2
+        // Random right number from 0 to 12
+        let rightNumber:Int = Int(arc4random_uniform(13))
         
-        rightImageView.image = UIImage(named: "card\(rightNumber)")
+        rightImageView.image = UIImage(named: cardNames[rightNumber])
         
+        
+        // Compare the card numbers
+        if leftNumber > rightNumber {
+            // Left wins
+            
+            // Increment score
+            leftScore += 1
+            
+            // Update label
+            leftScoreLabel.text = String(leftScore)
+        }
+        else if leftNumber == rightNumber {
+            // Tie
+            
+        }
+        else {
+            // Right wins
+            
+            // Increment score
+            rightScore += 1
+            
+            // Update label
+            rightScoreLabel.text = String(rightScore)
+        }
     }
     
     
